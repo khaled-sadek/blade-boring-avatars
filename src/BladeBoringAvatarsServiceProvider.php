4258e803053306d83eda220d3cd18284bcf8a4f0
Khaled Sadek <khaled@theplanet.tech>
2023-05-11 18:19:07 +0300

    support laravel 9 & 10

--- src/BladeBoringAvatarsServiceProvider.php
+++ src/BladeBoringAvatarsServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace KhaledSadek\BladeBoringAvatars;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\View\Compilers\BladeCompiler;
 use KhaledSadek\BladeBoringAvatars\Components\Avatar;
 
 class BladeBoringAvatarsServiceProvider extends ServiceProvider
@@ -26,8 +26,6 @@ class BladeBoringAvatarsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views/components', 'blade-boring-avatars');
-        $this->callAfterResolving(BladeCompiler::class, function(BladeCompiler $blade){
-            $blade->component(Avatar::class);
-        });
+        Blade::component('Avatar', Avatar::class);
     }
 }
