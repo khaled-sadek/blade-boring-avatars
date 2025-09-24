a075c02c2e4849403d050bb925806107269d8b32
Khaled Sadek <khaledsadek286@gmail.com>
2025-09-24 22:41:24 +0300

    fix deprications

--- src/Helper.php
+++ src/Helper.php
@@ -25,19 +25,50 @@ class Helper
         return $array[$number % count($array)];
     }
 
+    /**
+     * Get a unit value based on the input number and range.
+     *
+     * @return int A value between 0 and abs($range)-1, or its negative if conditions are met
+     *
+     * @throws \InvalidArgumentException If range is zero
+     */
+    public static function getDigit(int $number, int $ntn): int
+    {
+        if ($ntn < 0) {
+            return 0;
+        }
+
+        $numberStr = (string) abs($number);
+        $length = strlen($numberStr);
+        $digitPosition = $length - 1 - $ntn;
+
+        if ($digitPosition < 0 || $digitPosition >= $length) {
+            return 0;
+        }
+
+        return (int) $numberStr[$digitPosition];
+    }
+
     public static function getUnit(int $number, int $range, int $index = 0): int
     {
-        $value = $number % $range;
-        if ($index > 0 && self::getDigit($number, $index) % 2 === 0) {
-            return -$value;
+        if ($range === 0) {
+            throw new \InvalidArgumentException('Range must not be zero');
+        }
+
+        $absRange = abs($range);
+
+        // Integer-safe modulo calculation
+        $modulus = $number % $absRange;
+        $value = ($modulus + $absRange) % $absRange;
+
+        if ($index > 0) {
+            $digit = self::getDigit($number, $index);
+            if (($digit % 2) === 0) {
+                return -$value;
+            }
         }
 
         return $value;
-    }
-
-    public static function getDigit(int $number, int $ntn): int
-    {
-        return (int) floor(($number / pow(10, $ntn)) % 10);
     }
 
     public static function getContrast(string $color): string
